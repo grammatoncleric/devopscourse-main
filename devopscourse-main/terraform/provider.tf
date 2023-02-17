72130a1d583f71c505cf2427180bee83705162c8
Augustine O <okey.okore@gmail.com>
2023-02-17 07:01:36 +0100

    Update provider.tf
    added ROLE arn to the provider file

--- devopscourse-main/terraform/provider.tf
+++ devopscourse-main/terraform/provider.tf
@@ -1,3 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::660271822087:role/DemoRoleForEC2"
+    session_name = "my-session"
+  }
 }
