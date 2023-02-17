836a62967501b11af50100f55c34c3c662b6c0a3
Augustine O <okey.okore@gmail.com>
2023-02-17 20:11:19 +0100

    Update provider.tf

--- devopscourse-main/terraform/provider.tf
+++ devopscourse-main/terraform/provider.tf
@@ -1,7 +1,9 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
   assume_role {
     role_arn     = "arn:aws:iam::660271822087:role/DemoRoleForEC2"
-    session_name = "my-session"
+    session_name = "my-terraform-session"
+    external_id  = "my-external-id"
   }
+  aws_iam_instance_profile = "DemoRoleForEC2"
 }
